1164a8d313f548b5f57a7f28bf77eb5ca28a26d7
Sea <a468033339@qq.com>
2024-01-05 23:42:00 +0800

    无限随机技能更新（已测试）： 1. 整合了键盘快捷键mod的功能； 2. 增加了开关来控制是否消耗蓝魂。

--- RandomLearnSkillExt/LearnSkillMenuPatch.cs
+++ RandomLearnSkillExt/LearnSkillMenuPatch.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using DG.Tweening;
 using HarmonyLib;
 using UnityEngine;
@@ -9,45 +10,111 @@ namespace RandomLearnSkillExt
     [HarmonyPatch(typeof(MenuSkillLearn))]
     public class LearnSkillMenuPatch
     {
+
+        /**
+         * 是否开启蓝魂消耗
+         */
+        private static bool costOn = false;
+
+        /**
+         * 需要花费的蓝魂
+         */
+        private static int costVal = 0;
+
+        /**
+         * 增加键盘快捷键的支持（原本只支持手柄）
+         */
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        public static void PostUpdate(MenuSkillLearn __instance)
+        {
+            // 开启/关闭蓝魂消耗 -- 快捷键P
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                costOn = !costOn;
+            }
+
+            // 以下均是原版的触发代码，仅加了键盘的按键支持
+            if (__instance.skillRandomCount > 0 && (Input.GetKeyDown(KeyCode.E) || Singleton<GamepadBtn>.Instance.GetUpLikeYKeyDown()))
+            {
+                __instance.SkillReRandom();
+                return;
+            }
+            if (Input.GetKeyDown(KeyCode.F) || Singleton<GamepadBtn>.Instance.GetLeftLikeXKeyDown())
+            {
+                __instance.AbandonSkill();
+                return;
+            }
+            if (__instance.isNightmareBook && (Input.GetKeyDown(KeyCode.Q) || Singleton<GamepadBtn>.Instance.GetGamepadUIReturn()))
+            {
+                __instance.SubNightmareLevel();
+                return;
+            }
+        }
+
+        /**
+         * 初始化
+         */
         [HarmonyPatch("On")]
         [HarmonyPrefix]
         public static void PostOn(MenuSkillLearn __instance)
         {
-            LearnSkillMenuPatch.CostVal = 0;
-            ((CanvasGroup)AccessTools.Field(typeof(MenuSkillLearn), "refineButton").GetValue(__instance)).GetComponentInChildren<Text>().text = "重新随机技能";
+            costVal = 0;
+            ((CanvasGroup)AccessTools.Field(typeof(MenuSkillLearn), "refineButton").GetValue(__instance)).GetComponentInChildren<Text>().text = "随机技能-无消耗";
         }
 
+        /**
+         * 选择技能后消耗归0
+         */
         [HarmonyPatch("ClickSkillCard")]
         [HarmonyPrefix]
         public static void PostClick(MenuSkillLearn __instance)
         {
-            LearnSkillMenuPatch.CostVal = 0;
+            costVal = 0;
         }
 
+        /**
+         * 执行重新随机方法
+         */
         [HarmonyPatch("SkillReRandom")]
         [HarmonyPostfix]
         public static void PostReRandom(MenuSkillLearn __instance)
         {
-            object obj = AccessTools.Method(typeof(MenuSkillLearn), "IESkillReRandom", null, null).Invoke(__instance, new object[0]);
-            __instance.StartCoroutine((IEnumerator)obj);
+            // 未开启消耗 或 开启了且有足够的蓝魂
+            if (!costOn || PlayerAnimControl.instance.Souls >= costVal)
+            {
+                if (costOn)
+                {
+                    PlayerAnimControl.instance.Souls -= costVal;
+                }
+                object obj = AccessTools.Method(typeof(MenuSkillLearn), "IESkillReRandom", null, null).Invoke(__instance, new object[0]);
+                __instance.StartCoroutine((IEnumerator)obj);
+            }
+
         }
 
+        /**
+         * 随机按钮的展示
+         */
         [HarmonyPatch("SkillRandom")]
         [HarmonyPostfix]
         public static void PostRandomSkill(MenuSkillLearn __instance)
         {
-            CanvasGroup refineButton = (CanvasGroup)AccessTools.Field(typeof(MenuSkillLearn), "refineButton").GetValue(__instance);
-            DOTween.To(() => refineButton.alpha, delegate (float x)
+            costVal += costOn ? 100 : 0;
+            // 未开启消耗 或 开启了且有足够的蓝魂
+            if (!costOn || PlayerAnimControl.instance.Souls >= costVal)
             {
-                refineButton.alpha = x;
-            }, 1f, 0.2f).SetDelay(0.1f);
-            refineButton.blocksRaycasts = true;
-            refineButton.GetComponentInChildren<Text>().text = "重新随机一次技能";
-            __instance.skillRandomCount = 1;
-            refineButton.gameObject.SetActive(true);
+                CanvasGroup refineButton = (CanvasGroup)AccessTools.Field(typeof(MenuSkillLearn), "refineButton").GetValue(__instance);
+                DOTween.To(() => refineButton.alpha, delegate (float x)
+                {
+                    refineButton.alpha = x;
+                }, 1f, 0.2f).SetDelay(0.1f);
+                refineButton.blocksRaycasts = true;
+                refineButton.GetComponentInChildren<Text>().text = string.Format("花费{0}蓝魂，重新随机一次技能", costVal);
+                __instance.skillRandomCount = 1;
+                refineButton.gameObject.SetActive(true);
+            }
         }
-
-        public static int CostVal;
 
     }
 }
--- RandomLearnSkillExt/Properties/AssemblyInfo.cs
+++ RandomLearnSkillExt/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("RandomLearnSkillExt")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("暖雪-无限随机技能-DLC2修复")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RandomLearnSkillExt")]
